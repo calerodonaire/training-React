f3aec351a52ca447aa5408b372bf81cb091b4bb2
davidcalero <calerodonaire@gmail.com>
2022-12-06 11:43:41 +0100

    examples-components

--- components/src/App.js
+++ components/src/App.js
@@ -1,15 +1,14 @@
-
-// import './App.css';
-
+import './App.css';
+import Title from "./components/Title";
+import Paragraph from "./components/Paragraph";
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem aspernatur nam non saepe inventore, culpa vel odio dolorem amet rerum, perferendis ab voluptas, porro cupiditate hic incidunt cumque at quo. Perspiciatis temporibus sed hic eveniet autem odio iste enim, labore libero nobis expedita est laborum perferendis laudantium in cum eum.
-      </p>
+    <div className="main">
+      <Title></Title>
+      <Paragraph></Paragraph>
+      <Paragraph></Paragraph>
     </div>
   );
 }
 
 export default App;
-
---
+++ components/src/components/Paragraph.js
@@ -0,0 +1,14 @@
+import "./Paragraph.css";
+
+function Paragraph() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptate
+      dolorum eaque ea veniam eos libero quia temporibus, eum doloremque id
+      assumenda voluptatum repellendus quae fugit repudiandae rerum,
+      voluptatibus suscipit.
+    </p>
+  );
+}
+
+export default Paragraph;
