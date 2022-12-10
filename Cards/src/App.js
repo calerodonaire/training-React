611273e3705a6b51ad1ab0620f2fd9203f852f22
davidcalero <calerodonaire@gmail.com>
2022-12-10 14:13:07 +0100

    cards done and responsive

--- Cards/src/App.js
+++ Cards/src/App.js
@@ -1,77 +1,22 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faStar,
-  faCircleCheck,
-  faEnvelope,
-  faDollarSign,
-  faAddressBook,
-  faPhoneFlip,
-} from "@fortawesome/free-solid-svg-icons";
-
 import "./App.css";
 import CardWrapper from "./components/CardWrapper";
-import freelancer1 from "./assets/img/freelancer-1.jpg";
+import CardsContainer from "./components/CardsContainer";
+import CardImg from "./components/CardImg";
+import CardInfo from "./components/CardInfo";
+import { freelancers } from "./db/freelancers";
 function App() {
-  const freelancer = [
-    {
-      id: 0,
-      title: "MR",
-      lastName: "ROBERT",
-      role: "UI/UX Designer",
-      fee: 85,
-      reviews: 105,
-    },
-  ];
+  console.log(freelancers);
   return (
-    <div className="card-container">
-      <CardWrapper>
-        <div className="img-container">
-          <div className="img-wrapper">
-            <img src={freelancer1} alt="freelancer1" />
-            <span className="dot"></span>
-          </div>
-        </div>
-        <div className="info-container">
-          <h3>MR ROBERT</h3>
-          <p>UI/UX Designer</p>
-          <div className="hire">
-            <div>
-              Rate: <span className="price">$85/hr</span>
-            </div>
-            <button className="hire-button">Hire Me</button>
-          </div>
-          <div className="rating">
-            <div>5.0</div>
-            <div>
-              <FontAwesomeIcon icon={faStar} />
-              <FontAwesomeIcon icon={faStar} />
-              <FontAwesomeIcon icon={faStar} />
-              <FontAwesomeIcon icon={faStar} />
-              <FontAwesomeIcon icon={faStar} />
-            </div>
-            <div>(105 Reviews)</div>
-          </div>
-          <div className="icons">
-            <div className="verified">
-              <FontAwesomeIcon icon={faCircleCheck} />
-              <span>verified</span>
-            </div>
-            <span className="circle available">
-              <FontAwesomeIcon icon={faDollarSign} />
-            </span>
-            <span className="circle available">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </span>
-            <span className="circle not-available">
-              <FontAwesomeIcon icon={faAddressBook} />
-            </span>
-            <span className="circle not-available">
-              <FontAwesomeIcon icon={faPhoneFlip} />
-            </span>
-          </div>
-        </div>
-      </CardWrapper>
-    </div>
+    <CardsContainer>
+      {freelancers.map((freelancer, index) => {
+        return (
+          <CardWrapper key={index}>
+            <CardImg freelancer={freelancer}></CardImg>
+            <CardInfo freelancer={freelancer}></CardInfo>
+          </CardWrapper>
+        );
+      })}
+    </CardsContainer>
   );
 }
 
---
+++ Cards/src/db/freelancers.js
@@ -0,0 +1,123 @@
+import fl1 from "../assets/freelancers/freelancer-1.jpg";
+import fl2 from "../assets/freelancers/freelancer-2.jpg";
+import fl3 from "../assets/freelancers/freelancer-3.jpg";
+import fl4 from "../assets/freelancers/freelancer-8.jpg";
+import fl5 from "../assets/freelancers/freelancer-9.jpg";
+import fl6 from "../assets/freelancers/freelancer-10.jpg";
+import fl7 from "../assets/freelancers/freelancer-11.jpg";
+import fl8 from "../assets/freelancers/freelancer-12.jpg";
+import fl9 from "../assets/freelancers/freelancer-13.jpg";
+import fl10 from "../assets/freelancers/freelancer-14.jpg";
+import fl11 from "../assets/freelancers/freelancer-15.jpg";
+import fl12 from "../assets/freelancers/freelancer-16.jpg";
+
+export const freelancers = [
+  {
+    "id": 0,
+    "img": { path: fl1, text: "freelancer 1" },
+    "title": "Mr.",
+    "last_name": "ROBERT",
+    "role": "UI/UX Designer",
+    "fee": 85,
+    "reviews": 105,
+  },
+  {
+    "id": 1,
+    "img": { path: fl2, text: "freelancer 2" },
+    "title": "Mrs",
+    "last_name": "Gitthouse",
+    "role": "Environmental Tech",
+    "fee": 73,
+    "reviews": 115,
+  },
+  {
+    "id": 2,
+    "img": { path: fl3, text: "freelancer 3" },
+    "title": "Mr",
+    "last_name": "Blumire",
+    "role": "Professor",
+    "fee": 84,
+    "reviews": 79,
+  },
+  {
+    "id": 3,
+    "img": { path: fl4, text: "freelancer 4" },
+    "title": "Ms",
+    "last_name": "Dinsmore",
+    "role": "Help Desk Technician",
+    "fee": 68,
+    "reviews": 92,
+  },
+  {
+    "id": 4,
+    "img": { paht: fl5, text: "freelancer 5" },
+    "title": "Rev",
+    "last_name": "Slesser",
+    "role": "Senior Cost Accountant",
+    "fee": 73,
+    "reviews": 77,
+  },
+  {
+    "id": 5,
+    "img": { path: fl6, text: "freelancer 6" },
+    "title": "Mr",
+    "last_name": "Thibodeaux",
+    "role": "Administrative Assistant II",
+    "fee": 83,
+    "reviews": 78,
+  },
+  {
+    "id": 6,
+    "img": { path: fl7, text: "freelancer 7" },
+    "title": "Dr",
+    "last_name": "Hriinchenko",
+    "role": "Engineer IV",
+    "fee": 75,
+    "reviews": 109,
+  },
+  {
+    "id": 7,
+    "img": { path: fl8, text: "freelancer 8" },
+    "title": "Dr",
+    "last_name": "Dullingham",
+    "role": "Recruiter",
+    "fee": 68,
+    "reviews": 89,
+  },
+  {
+    "id": 8,
+    "img": { path: fl9, text: "freelancer 9" },
+    "title": "Mr",
+    "last_name": "Santori",
+    "role": "Help Desk Operator",
+    "fee": 71,
+    "reviews": 82,
+  },
+  {
+    "id": 9,
+    "img": { path: fl10, text: "freelancer 10" },
+    "title": "Mrs",
+    "last_name": "Ivanchenkov",
+    "role": "Biostatistician I",
+    "fee": 72,
+    "reviews": 115,
+  },
+  {
+    "id": 10,
+    "img": { path: fl11, text: "freelancer 11" },
+    "title": "Dr",
+    "last_name": "Voice",
+    "role": "Librarian",
+    "fee": 85,
+    "reviews": 85,
+  },
+  {
+    "id": 11,
+    "img": { path: fl12, text: "Freelancer 12" },
+    "title": "Mrs",
+    "last_name": "Alloway",
+    "role": "Product Engineer",
+    "fee": 84,
+    "reviews": 94,
+  },
+];
