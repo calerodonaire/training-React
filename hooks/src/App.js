a00f5842a319f1b8fc3fd88ae6962523416623e8
davidcalero <calerodonaire@gmail.com>
2022-12-11 12:31:46 +0100

    contador de clicks

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,14 +1,35 @@
-// import './App.css';
+import "./App.css";
 import { useState } from "react";
-import Person from "./components/Person";
+// import Person from "./components/Person";
+import Counter from "./components/Counter";
 function App() {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const handlePlusOne = () => {
+    setCount(count + 1);
+  };
+  const handleTimesFive = () => {
+    setCount(count * 5);
+  };
+  const handleMinusOne = () => {
+    setCount(count - 1);
+  };
+  const handleDivideByHundred = () => {
+    if (count !== 0) {
+      setCount(count / 100);
+    } else {
+      console.log("División de 0 no es posible");
+    }
+  };
 
   return (
     <div>
-      <Person></Person>
-      {/* <p>Has clicado {count} veces.</p>
-      <button onClick={() => setCount(count + 1)}>Pulsame</button> */}
+      <Counter count={count} />
+      <button onClick={handlePlusOne}>Pulsame para sumar</button>
+      <button onClick={handleTimesFive}>Pulsame para multiplicar</button>
+      <button onClick={handleMinusOne}>Pulsame para restar</button>
+      <button onClick={handleDivideByHundred}>
+        Pulsame para dividir por 100
+      </button>
     </div>
   );
 }
---
+++ hooks/src/components/Counter.js
@@ -0,0 +1,7 @@
+
+
+function Counter(props) {
+  return <div className="contador"> Has clicado{props.count} veces</div>;
+}
+
+export default Counter;
